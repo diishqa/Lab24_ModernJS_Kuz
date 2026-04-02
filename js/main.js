db22871c17356909e67de557c4bbb3df2d7eb788
diishqa <diana.34@list.ru>
2026-04-02 21:19:16 +0300

    promises in JS

--- js/main.js
+++ js/main.js
@@ -108,3 +108,80 @@
 // import * as Utils from "./utils.js";
 // console.log(Utils.greet("Динара"));
 // console.log("Умножить:", Utils.multiply(3,9));
+// import {square, cube, E} from './math.js';
+// console.log("Квадрат числа 5:", square(5));
+// console.log("Куб числа 3:", cube(3));
+// console.log("Константа E:", E);
+console.log("Промисы");
+const simplePromise = new Promise ((resolve, reject) => {
+    const success = true;
+    if (success) {
+        resolve("Операция выполнена успешно!");
+    } else {
+        reject("Произошла ошибка!")
+    }
+});
+simplePromise
+    .then((result) => console.log("Результат:", result))
+    .catch((error) => console.log("Ошибка:", error));
+function delay(ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Прошло ${ms} миллисекунд`);
+        }, ms);
+    });
+}
+delay(1000)
+    .then((message) => console.log(message));
+function fetchUserData(userId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (userId >0) {
+                resolve({
+                    id: userId,
+                    name: "Диана Кузьмина",
+                    email: "adasdas@mail.com",
+                });
+            } else{
+                reject("Неверный ID пользователя");
+            }
+        }, 1500)
+    })
+}
+fetchUserData(1)
+    .then((user) => console.log("Пользователб:", user))
+    .catch((error) => console.log("Ошибка:", error));
+function step1() {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve ("Шаг 1 завершен"), 500);
+    });
+}
+function step2(previousResult) {
+    return new Promise((resolve) => {
+        setTimeout(()=> resolve(`${previousResult} -> Шаг 2 завершен`), 500);
+    });
+}
+function step3 (previousResult) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(`${previousResult} -> Шаг 3 завершен`), 500);
+    });
+}
+step1()
+    .then((result1) => step2(result1))
+    .then((result2) => step3(result2))
+    .then((finalResult) => console.log("Финальный результат:", finalResult))
+    .catch((error) => console.log("Ошибка в цепочке:", error));
+function checkInventory(productName, inStock) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(inStock) {
+                resolve(`ТОвар ${productName} в наличии`);
+            } else {
+                reject(`Товар ${productName} не в наличии`);
+            }
+        }, 1000)
+    })
+}
+checkInventory("Кошелек", true)
+    .then((message) => console.log("В наличии:", message))
+    .catch((error) => console.log("Ошибка:", error));
